f701b8e55eb3950d9dc3a56e2d82c0357899106a
Liam Dickson <liam@Liams-MacBook-Air.local>
2015-04-20 13:09:39 -0400

    working output

--- phonebooker.py
+++ phonebooker.py
@@ -1,24 +1,30 @@
 #!/usr/local/bin/python
 import sys
 import re
+import csv
 import urllib
 import urllib3
 
-full_name = ''
-for i in xrange(1,len(sys.argv)):
-    full_name += sys.argv[i] + ' '
-name = full_name.replace(' ','%20')
-url = 'http://phonebook.uconn.edu/results.php?basictext=' + name
+with open(sys.argv[1]) as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for i,row in enumerate(reader):
+        if i != 0:
+            full_name = ''
+            full_name += row[0] + ' '
+            full_name += row[2]
 
-http = urllib3.PoolManager()
-r = http.request('GET', url)
-page_text = r.data
+            name = full_name.replace(' ','%20')
+            url = 'http://phonebook.uconn.edu/results.php?basictext=' + name
 
-if 'subHead2' in page_text:
-    print full_name[:-1] + " is a student."
-elif '<div align="left">Status</div>' in page_text:
-    print "Too many results."
-elif 'records than the maximum allowed' in page_text:
-    print "Way too many results."
-else:
-    print "No results."
+            http = urllib3.PoolManager()
+            r = http.request('GET', url)
+            page_text = r.data
+
+            sys.stdout.write(row[0] + ',' + row[2] + ',')
+            if 'subHead2' in page_text:
+                print 'Student'
+            elif('<div align="left">Status</div>' in page_text or
+                'records than the maximum allowed' in page_text):
+                print "Multiple"
+            else:
+                print "Not Student"
